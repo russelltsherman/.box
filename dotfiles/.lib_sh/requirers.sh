a0979e7103e8bce47491bb0f55c9bf829151f49e
Russell Sherman <russell.t.sherman@gmail.com>
2017-01-30 22:42:18 -0600

    make require brew and cask report existing properly

--- dotfiles/.lib_sh/requirers.sh
+++ dotfiles/.lib_sh/requirers.sh
@@ -7,8 +7,8 @@
 
 function require_cask() {
     running "brew cask $1"
-    brew cask list $1 > /dev/null 2>&1 | true
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+    brew cask list $1 > /dev/null 2>&1
+    if [[ "$?" != "0" ]]; then
         action "brew cask install $1 $2"
         brew cask install $1
         if [[ $? != 0 ]]; then
@@ -21,8 +21,8 @@ function require_cask() {
 
 function require_brew() {
     running "brew $1 $2"
-    brew list $1 > /dev/null 2>&1 | true
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
+    brew list $1 > /dev/null 2>&1
+    if [[ "$?" != "0" ]]; then
         action "brew install $1 $2"
         brew install $1 $2
         if [[ $? != 0 ]]; then
