a06d41d76301eb5cac61c35f32b283c3ca64769f
Russ <rsherman@kabam.com>
2017-02-02 13:58:26 -0600

    smaller history

--- dotfiles/.zsh/history.zsh
+++ dotfiles/.zsh/history.zsh
@@ -3,5 +3,5 @@
 ##############################################################################
 HISTSIZE=1000               #How many lines of history to keep in memory
 HISTFILE=~/.zsh_history     #Where to save history to disk
-SAVEHIST=10000              #Number of history entries to save to disk
+SAVEHIST=1000               #Number of history entries to save to disk
 HISTDUP=erase               #Erase duplicates in the history file
