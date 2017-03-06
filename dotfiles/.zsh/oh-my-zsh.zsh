a11b990e134b6d87aabad67463ea23ef610f28ce
Russell <russell.t.sherman@gmail.com>
2017-03-06 14:36:03 -0600

    dynamic reference antibody home path

--- dotfiles/.zsh/oh-my-zsh.zsh
+++ dotfiles/.zsh/oh-my-zsh.zsh
@@ -3,7 +3,7 @@
 
 # Path to your oh-my-zsh installation.
 # export ZSH=$HOME/.oh-my-zsh
-export ZSH=$HOME/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/
+export ZSH="$(antibody home)/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/"
 
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
